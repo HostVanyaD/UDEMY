c52ad822838cb4ada06a8238636afc5be27c4884
HostVanyaD <reeedruuum@gmail.com>
2022-07-04 14:45:12 +0300

    Add sliding

--- UDEMY-The Complete JS Course Projects/Bankist marketing page/script.js
+++ UDEMY-The Complete JS Course Projects/Bankist marketing page/script.js
@@ -132,3 +132,75 @@ allSections.forEach(function (section) {
   section.classList.add('section--hidden');
 })
 
+///////////////////////////////////////
+// Slider
+const slider = function () {
+  const slides = document.querySelectorAll('.slide');
+  const btnLeft = document.querySelector('.slider__btn--left');
+  const btnRight = document.querySelector('.slider__btn--right');
+  const dotsContainer = document.querySelector('.dots');
+
+  let currSlide = 0;
+  const maxSlide = slides.length - 1;
+
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotsContainer.insertAdjacentHTML(
+        'beforeend',
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll('.dots__dot')
+      .forEach(dot => dot.classList.remove('dots__dot--active'));
+    
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add('dots__dot--active');
+  };
+
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
+
+  // Starting point
+  const init = function () {
+    goToSlide(0);
+
+    createDots();
+    activateDot(0);
+  };
+  init();
+
+  // Moving to Next slide
+  const nextSlide = function () {
+    if (currSlide === maxSlide) currSlide = 0;
+    else currSlide++;
+
+    goToSlide(currSlide);
+    activateDot(currSlide);
+  };
+
+  // Moving to Previous slide
+  const prevSlide = function () {
+    if (currSlide === 0) currSlide = maxSlide;
+    else currSlide--;
+
+    goToSlide(currSlide);
+    activateDot(currSlide);
+  };
+
+  btnRight.addEventListener('click', nextSlide);
+  btnLeft.addEventListener('click', prevSlide);
+
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'ArrowLeft') prevSlide();
+    if (e.key === 'ArrowRight') nextSlide();
+  });
+};
+slider();
